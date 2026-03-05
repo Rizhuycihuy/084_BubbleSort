1ee99ab34d354463d395aa2f92f8140abdf4412b
Rizhuycihuy <rizki003131@gmail.com>
2026-03-05 17:44:48 +0700

    menambahkan input

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -5,3 +5,21 @@ using namespace std;
 int arr[20];
 int n;
 
+void input()
+{
+  int d;
+  while (true)
+  {
+      cout << "masukkan banyak nya elemen pada array : ";
+      cin >> n;
+      if (n <= 20)
+          break;
+      else
+      {
+          cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+      }
+  }
+  cout << endl;
+  cout << "====================" << endl;
+} 
+
