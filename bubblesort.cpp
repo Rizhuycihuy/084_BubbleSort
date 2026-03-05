94c6bae0118b8129e10a0bf0b0c102a7f5a54816
Rizhuycihuy <rizki003131@gmail.com>
2026-03-05 18:43:02 +0700

    add display

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -58,5 +58,35 @@ void bubblesortarray()
       
       } while (pass <= n - 1);
 }
+void display()
+{
+  cout << endl;
+  cout << "==================================" << endl;
+  cout << "Element array yang telah tersusun" << endl;
+  cout << "==================================" << endl;
+  cout << endl;
+  for (int  j = 0; j < n; j++)
+  {
+    cout << arr[j];
+    if (j < n - 1)
+    {
+        cout << " --> ";
+    
+    }
+  }
+  cout << endl;
+  cout << endl;
 
+  cout << "jumlah pass = " << n - 1 << endl;
+  cout << endl;
+  cout << endl;
+} 
+
+int main()
+{
+  input();
+
+  bubblesortarray();
+  display();
+}
 
