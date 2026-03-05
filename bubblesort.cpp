e3d9162ce6c8d27d1be8df5748db52f4c857cfb6
Rizhuycihuy <rizki003131@gmail.com>
2026-03-05 17:54:53 +0700

    add input

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -10,10 +10,10 @@ void input()
   int d;
   while (true)
   {
-      cout << "masukkan banyak nya elemen pada array : ";
-      cin >> n;
-      if (n <= 20)
-          break;
+      cout << "masukkan banyak nya elemen pada array : "; // output ke layar
+      cin >> n;                                           // input dari penguna
+      if (n <= 20)                                        // jika n kurang dari atau sama dengan 20
+          break;                                          // keluar dari loop
       else
       {
           cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
@@ -21,5 +21,14 @@ void input()
   }
   cout << endl;
   cout << "====================" << endl;
+  cout << "masukkan elemen array" << endl;
+  cout << "====================" << endl;
+
+  for (int i = 0; i < n; i++)
+  {                                           // looping dengan i di mulai dari 0 
+      cout << "Data ke-" << (i + 1) << ": ";  // output ke layar
+      cin >> arr[i];                          // input dari pengguna
+  }
 } 
+
 
